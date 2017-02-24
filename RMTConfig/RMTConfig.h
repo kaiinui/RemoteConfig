ded20fcae12bc65e98bb982bcb21cbece1dee5d6
Kai INUI <lied.der.optik@gmail.com>
2017-02-24 15:14:17 +0900

    Carthage

---
+++ RMTConfig/RMTConfig.h
@@ -0,0 +1,19 @@
+//
+//  RMTConfig.h
+//  RMTConfig
+//
+//  Created by kai inui on 2017/02/24.
+//  Copyright © 2017年 kotori. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RMTConfig.
+FOUNDATION_EXPORT double RMTConfigVersionNumber;
+
+//! Project version string for RMTConfig.
+FOUNDATION_EXPORT const unsigned char RMTConfigVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RMTConfig/PublicHeader.h>
+
+
---
+++ RMTConfigTests/RMTConfigTests.m
@@ -0,0 +1,39 @@
+//
+//  RMTConfigTests.m
+//  RMTConfigTests
+//
+//  Created by kai inui on 2017/02/24.
+//  Copyright © 2017年 kotori. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RMTConfigTests : XCTestCase
+
+@end
+
+@implementation RMTConfigTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
